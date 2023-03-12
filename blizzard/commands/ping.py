8769039ff0df99da2c7593b13e535f66dbf8e510
xsnowstorm <118460071+xsnowstorm@users.noreply.github.com>
2023-03-12 09:38:10 +0000

    removed blizzard directory

--- blizzard/commands/ping.py
+++
@@ -1,8 +0,0 @@
-from blizzard import Command
-
-class Cmd(Command):
-    name = "ping"
-    description = "Ping command"
-
-    async def execute(self, message, arguments):
-        await message.reply("Pong!")
